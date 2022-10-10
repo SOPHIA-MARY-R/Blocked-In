5e826edb018d0b779a33e9ac6eefe8eb6162d628
Subhasree M <subh20413.cs@rmkec.ac.in>
2022-10-10 19:52:24 +0530

    Added Livereload

--- BlockedIn/BlockedIn/settings.py
+++ BlockedIn/BlockedIn/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
     'authentication',
     'timer'
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'BlockedIn.urls'
