7580a13c078a74e1b43b08b9ca55675e1245af1b
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2022-11-01 15:44:30 +0530

    added home page

--- BlockedIn/BlockedIn/settings.py
+++ BlockedIn/BlockedIn/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'livereload',
     'django.contrib.staticfiles',
     'authentication',
-    'timer'
+    'timer',
 ]
 
 MIDDLEWARE = [
@@ -121,9 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
