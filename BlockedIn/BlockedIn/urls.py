2b5c7e3c7c753b2c9a8eb742f1abcd89e081db24
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2022-11-07 16:36:23 +0530

    Modified urls.py

--- BlockedIn/BlockedIn/urls.py
+++ BlockedIn/BlockedIn/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('authentication.urls')),
     path('extension/', include('block.urls')),
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
